6066dacf212e11fdaceb497a5d07e2476a892501
YiipHuhu <alan.gabriel@nullbug.dev>
2024-08-22 20:49:44 -0300

    subindo pequenas alterações no apllication controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  protected
+
+  def record_not_found(error)
+    render json: { errors: [error.message] }, status: :not_found
+  end
 end
