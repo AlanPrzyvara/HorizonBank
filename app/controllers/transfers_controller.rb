37097d12c4640aa97d4c1b0f8443cabda82afcdc
YiipHuhu <alan.gabriel@nullbug.dev>
2024-08-23 23:22:56 -0300

    adicionado registros de transferencia bancaria entre contas

--- app/controllers/transfers_controller.rb
+++ app/controllers/transfers_controller.rb
@@ -2,21 +2,43 @@ class TransfersController < ApplicationController
   def create
     sender_account = Account.find(params[:account_id])
     receiver_account = Account.find(transfer_params[:receiver_account_id])
+    amount = transfer_params[:amount].to_d
 
-    if sender_account.balance < transfer_params[:amount].to_d
+    if sender_account.balance < amount
       render json: { error: 'Saldo insuficiente para a transação' }, status: :unprocessable_entity
       return
     end
 
     ActiveRecord::Base.transaction do
-      sender_account.update!(balance: sender_account.balance - transfer_params[:amount].to_d)
-      receiver_account.update!(balance: receiver_account.balance + transfer_params[:amount].to_d)
+      balance_before_sender = sender_account.balance
+      balance_after_sender = balance_before_sender - amount
+      balance_before_receiver = receiver_account.balance
+      balance_after_receiver = balance_before_receiver + amount
+
+      sender_account.update!(balance: balance_after_sender)
+      receiver_account.update!(balance: balance_after_receiver)
 
       @transfer = Transfer.create!(
         sender_account: sender_account,
         receiver_account: receiver_account,
-        amount: transfer_params[:amount],
+        amount: amount,
         transfer_type: 'transfer'
+      )
+
+      # Cria uma transação para o remetente
+      sender_account.transactions.create!(
+        transaction_type: 'debit',
+        amount: amount,
+        balance_before: balance_before_sender,
+        balance_after: balance_after_sender
+      )
+
+      # Cria uma transação para o destinatário
+      receiver_account.transactions.create!(
+        transaction_type: 'credit',
+        amount: amount,
+        balance_before: balance_before_receiver,
+        balance_after: balance_after_receiver
       )
     end
 
