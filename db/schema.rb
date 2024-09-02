b904cc94398b1173fe61301d82d1c7b99d3b309f
YiipHuhu <alan.gabriel@nullbug.dev>
2024-09-01 22:38:46 -0300

    add mudanças no schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,16 +1,16 @@
-# Este arquivo é gerado automaticamente a partir do estado atual do banco de dados. Em vez
-# de editar este arquivo, por favor, use o recurso de migrações do Active Record para
-# modificar incrementalmente seu banco de dados e, em seguida, regenerar esta definição de esquema.
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
 #
-# Este arquivo é usado pelo Rails para definir seu esquema ao executar `bin/rails
-# db:schema:load`. Ao criar um novo banco de dados, `bin/rails db:schema:load` tende a
-# ser mais rápido e potencialmente menos propenso a erros do que executar todas as suas
-# migrações do zero. Migrações antigas podem falhar em ser aplicadas corretamente se essas
-# migrações usarem dependências externas ou código da aplicação.
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
-# É altamente recomendado que você faça o commit deste arquivo em seu sistema de controle de versão.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_18_233851) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_25_023412) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,52 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_18_233851) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+  end
+
+  create_table "companies", force: :cascade do |t|
+    t.string "company_name"
+    t.string "fantasy_name"
+    t.string "cnpj", null: false
+    t.date "opening_date"
+    t.string "legal_nature"
+    t.string "primary_activity_code"
+    t.string "primary_activity_text"
+    t.string "situation"
+    t.string "address"
+    t.string "number"
+    t.string "neighborhood"
+    t.string "city"
+    t.string "state"
+    t.string "zip_code"
+    t.string "phone"
+    t.decimal "capital_social", default: "0.0"
+    t.decimal "balance"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cnpj"], name: "index_companies_on_cnpj", unique: true
+  end
+
+  create_table "companys", force: :cascade do |t|
+    t.string "company_name"
+    t.string "fantasy_name"
+    t.string "cnpj"
+    t.date "opening_date"
+    t.string "legal_nature"
+    t.string "primary_activity_code"
+    t.string "primary_activity_text"
+    t.string "situation"
+    t.string "address"
+    t.string "number"
+    t.string "neighborhood"
+    t.string "city"
+    t.string "state"
+    t.string "zip_code"
+    t.string "phone"
+    t.decimal "capital_social"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.decimal "balance"
   end
 
   create_table "transactions", force: :cascade do |t|
