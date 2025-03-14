065ae291e3d3a83df8d92489873f53b2d6c9869a
YiipHuhu <alan.gabriel@nullbug.dev>
2025-03-14 00:00:22 -0300

    validacao de token valido

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-
+  before_action :authenticate_account
   protected
 
   def record_not_found(error)
@@ -8,4 +8,22 @@ class ApplicationController < ActionController::Base
   end
   rescue => e
     render json: { errors: e.message }, status: :internal_server_error
+
+    private
+
+    def authenticate_account
+      token = request.headers['Authorization']&.split(' ')&.last
+      return render json: { error: "Token não fornecido" }, status: :unauthorized unless token
+
+      begin
+        decoded_token = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+        @current_account = Account.find(decoded_token['account_id'])
+      rescue JWT::DecodeError
+        render json: { error: "Token inválido" }, status: :unauthorized
+      end
+    end
+
+    def current_account
+      @current_account
+    end
 end
