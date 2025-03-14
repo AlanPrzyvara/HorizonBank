c0b34700dae485e8554389efa52f5ff663997243
YiipHuhu <alan.gabriel@nullbug.dev>
2025-03-14 00:38:04 -0300

    ajuste de bug do controller nao encontrado

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
   resources :accounts, only: [:create, :show, :index] do
-    resources :transactions, only: [:create,:show, :index]
+    resources :transactions, only: [:create, :show, :index]
     resources :transfers, only: [:create]
   end
 end
